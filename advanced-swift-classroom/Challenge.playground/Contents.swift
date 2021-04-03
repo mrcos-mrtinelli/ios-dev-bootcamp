89f7936559a74295029ab76fcff4f56180a63ff5
mrcos <m2e5090@gmail.com>
2021-04-03 11:57:06 -0700

    update challenge

--- advanced-swift-classroom/Challenge.playground/Contents.swift
+++ advanced-swift-classroom/Challenge.playground/Contents.swift
@@ -1,6 +1,6 @@
 import Foundation
 
-// how much paint is needed to paint the wall?
+// Part 1 - how much paint is needed to paint the wall?
 // each bucket cover 1.5 meters squared
 // * measurements are metric
 
@@ -9,11 +9,20 @@ var height: Float = 2.3
 var areaCoveredPerBucket: Float = 1.5
 
 var numberOfBuckets: Int {
-    
+    // part 1
     get {
         let area = width * height
-        return Int(area / areaCoveredPerBucket)
+        let result = area / areaCoveredPerBucket
+        return Int(ceilf(result))
+    }
+    // part 2
+    set {
+        let totalAreaCovered = areaCoveredPerBucket * Float(newValue)
+        print("you can paint a total of \(totalAreaCovered) meters squared")
     }
 }
 
 print("you need \(numberOfBuckets) buckets to paint the wall")
+
+// Part 2 - what is the total area that can be covered.
+numberOfBuckets = 3
