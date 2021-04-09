2ababc7a0e336d5eb16060cff6aa70fde4fb74c3
mrcos <m2e5090@gmail.com>
2021-04-08 19:51:03 -0700

    add model

--- NameThatFlower/NameThatFlower/ViewController.swift
+++ NameThatFlower/NameThatFlower/ViewController.swift
@@ -6,6 +6,8 @@
 //
 
 import UIKit
+import CoreML
+import Vision
 
 class ViewController: UIViewController {
     
@@ -22,12 +24,48 @@ class ViewController: UIViewController {
         
         imagePicker.delegate = self
         imagePicker.sourceType = .photoLibrary
-        imagePicker.allowsEditing = false
+        imagePicker.allowsEditing = true
     }
     
     //MARK: Navigation Bar
     @objc func cameraTapped() {
+        
         present(imagePicker, animated: true)
+    
+    }
+    
+    //MARK: Process Method
+    func process(image: CIImage) {
+       
+        // create model
+        guard let model = try? VNCoreMLModel(for: FlowerClassification(configuration: MLModelConfiguration()).model) else {
+            fatalError("Unable to process CIImage")
+        }
+        
+        // create a request
+        let request = VNCoreMLRequest(model: model) { (req, err) in
+            
+            guard let results = req.results as? [VNClassificationObservation] else {
+                fatalError("error getting results")
+            }
+            
+            if let firstResult = results.first {
+                self.title = firstResult.identifier.capitalized
+            }
+        }
+        
+        // create a handler
+        let handler = VNImageRequestHandler(ciImage: image)
+        
+        // perform request
+        do {
+            
+            try handler.perform([request])
+            
+        } catch  {
+            fatalError("could not perform request")
+        }
+        
     }
 
 }
@@ -39,8 +77,14 @@ extension ViewController: UIImagePickerControllerDelegate, UINavigationControlle
             fatalError("unable to get image")
         }
         
+        guard let ciImage = CIImage(image: image) else {
+            fatalError("unable to convert UIImage to CIIage")
+        }
+        
         imageView.image = image
         
         imagePicker.dismiss(animated: true)
+        
+        process(image: ciImage)
     }
 }
