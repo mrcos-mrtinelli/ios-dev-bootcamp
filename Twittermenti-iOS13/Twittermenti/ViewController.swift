bbc603e2dc3aaf660e4ae2b609755ac4c4dbcbca
mrcos <m2e5090@gmail.com>
2021-04-13 21:10:26 -0700

    initialize swifter with keys (not included)

--- Twittermenti-iOS13/Twittermenti/ViewController.swift
+++ Twittermenti-iOS13/Twittermenti/ViewController.swift
@@ -7,15 +7,20 @@
 //
 
 import UIKit
+import SwifteriOS
 
 class ViewController: UIViewController {
     
     @IBOutlet weak var backgroundView: UIView!
     @IBOutlet weak var textField: UITextField!
     @IBOutlet weak var sentimentLabel: UILabel!
+    
+    let swifter = Swifter(consumerKey: "", consumerSecret: "")
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+//        swifter.searchTweet(using: <#T##String#>, geocode: <#T##String?#>, lang: <#T##String?#>, locale: <#T##String?#>, resultType: <#T##String?#>, count: <#T##Int?#>, until: <#T##String?#>, sinceID: <#T##String?#>, maxID: <#T##String?#>, includeEntities: <#T##Bool?#>, callback: <#T##String?#>, tweetMode: <#T##TweetMode#>, success: <#T##Swifter.SearchResultHandler?##Swifter.SearchResultHandler?##(JSON, JSON) -> Void#>, failure: <#T##Swifter.FailureHandler##Swifter.FailureHandler##(Error) -> Void#>)
         
     }
 
