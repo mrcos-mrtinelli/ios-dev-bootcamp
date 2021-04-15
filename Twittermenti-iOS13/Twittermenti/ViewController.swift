725317f96eff5d58792f0af120dec042c8690e64
mrcos <m2e5090@gmail.com>
2021-04-14 20:53:01 -0700

    update swifter.searchTweet

--- Twittermenti-iOS13/Twittermenti/ViewController.swift
+++ Twittermenti-iOS13/Twittermenti/ViewController.swift
@@ -15,13 +15,19 @@ class ViewController: UIViewController {
     @IBOutlet weak var textField: UITextField!
     @IBOutlet weak var sentimentLabel: UILabel!
     
-    let swifter = Swifter(consumerKey: "", consumerSecret: "")
+    let swifter = Swifter(consumerKey: "869xR38uJr7YL4k6Lt30Lb7bz", consumerSecret: "IgwMAri9VtbSSGDLqZSGPB7A8HG1Yna4Edg05eFXW8YMv4HwHJ")
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-//        swifter.searchTweet(using: <#T##String#>, geocode: <#T##String?#>, lang: <#T##String?#>, locale: <#T##String?#>, resultType: <#T##String?#>, count: <#T##Int?#>, until: <#T##String?#>, sinceID: <#T##String?#>, maxID: <#T##String?#>, includeEntities: <#T##Bool?#>, callback: <#T##String?#>, tweetMode: <#T##TweetMode#>, success: <#T##Swifter.SearchResultHandler?##Swifter.SearchResultHandler?##(JSON, JSON) -> Void#>, failure: <#T##Swifter.FailureHandler##Swifter.FailureHandler##(Error) -> Void#>)
-        
+        swifter.searchTweet(using: "@Apple", lang: "en", count: 100, tweetMode: .extended) { (results, metadata) in
+            print("results: \(results)")
+            print("metadata: \(metadata)")
+            
+        } failure: { (error) in
+            print("error: \(error)")
+        }
+
     }
 
     @IBAction func predictPressed(_ sender: Any) {
