8ede2d16eabaeefe1977410e60e5a66e829136bf
mrcos <m2e5090@gmail.com>
2021-04-19 16:12:43 -0700

    add dice and grid

--- ARDiceee/ARDiceee/ViewController.swift
+++ ARDiceee/ARDiceee/ViewController.swift
@@ -16,6 +16,8 @@ class ViewController: UIViewController, ARSCNViewDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        sceneView.debugOptions = [ARSCNDebugOptions.showFeaturePoints]
+        
         // Set the view's delegate
         sceneView.delegate = self
         
@@ -24,45 +26,35 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         
         // create cube using SceneKit's methods
 //        let cube = SCNBox(width: 0.1, height: 0.1, length: 0.1, chamferRadius: 0.001)
-        
-        let sphere = SCNSphere(radius: 0.1)
-        
+//        let sphere = SCNSphere(radius: 0.1)
         // add some color to the cube
 //        let cubeMaterial = SCNMaterial()
-        
-        let sphereMaterial = SCNMaterial()
-        
+//        let sphereMaterial = SCNMaterial()
 //        cubeMaterial.diffuse.contents = UIColor.red
-        
-        sphereMaterial.diffuse.contents = UIImage(named: "art.scnassets/Mars.jpg")
-        
+//        sphereMaterial.diffuse.contents = UIImage(named: "art.scnassets/Mars.jpg")
 //        cube.materials = [cubeMaterial]
-        
-        sphere.materials = [sphereMaterial]
-        
+//        sphere.materials = [sphereMaterial]
 //        let cubeNode = SCNNode()
-        
-        let sphereNode = SCNNode()
-        
+//        let sphereNode = SCNNode()
 //        cubeNode.position = SCNVector3(0, 0.1, -0.5)
-        
 //        cubeNode.geometry = cube
-        
-        sphereNode.position = SCNVector3(0.2, 0.1, -0.5)
-        
-        sphereNode.geometry = sphere
-        
+//        sphereNode.position = SCNVector3(0.2, 0.1, -0.5)
+//        sphereNode.geometry = sphere
 //        sceneView.scene.rootNode.addChildNode(cubeNode)
-        
-        sceneView.scene.rootNode.addChildNode(sphereNode)
+//        sceneView.scene.rootNode.addChildNode(sphereNode)
 
         sceneView.autoenablesDefaultLighting = true
         
         // Create a new scene
-//        let scene = SCNScene(named: "art.scnassets/ship.scn")!
+        let diceScene = SCNScene(named: "art.scnassets/diceCollada.scn")!
+        
+        if let diceNode = diceScene.rootNode.childNode(withName: "Dice", recursively: true) {
+            diceNode.position = SCNVector3(x: 0, y: 0, z: -0.1)
+            
+            sceneView.scene.rootNode.addChildNode(diceNode)
+        }
         
         // Set the scene to the view
-//        sceneView.scene = scene
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -70,6 +62,8 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         
         // Create a session configuration
         let configuration = ARWorldTrackingConfiguration()
+        
+        configuration.planeDetection = .horizontal
 
         // Run the view's session
         sceneView.session.run(configuration)
@@ -80,6 +74,34 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         
         // Pause the view's session
         sceneView.session.pause()
+    }
+    
+    func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
+        if anchor is ARPlaneAnchor {
+            
+            let planeAnchor = anchor as! ARPlaneAnchor
+            
+            let plane = SCNPlane(width: CGFloat(planeAnchor.extent.x), height: CGFloat(planeAnchor.extent.z))
+            
+            let planeNode = SCNNode()
+            
+            planeNode.position = SCNVector3(x: planeAnchor.center.x, y: 0, z: planeAnchor.center.z)
+            
+            planeNode.transform = SCNMatrix4MakeRotation(-Float.pi / 2, 1, 0, 0)
+            
+            let gridMaterial = SCNMaterial()
+            
+            gridMaterial.diffuse.contents = UIImage(named: "art.scnassets/grid.png")
+            
+            plane.materials = [gridMaterial]
+            
+            planeNode.geometry = plane
+            
+            node.addChildNode(planeNode)
+            
+        } else {
+            return
+        }
     }
 
 }
